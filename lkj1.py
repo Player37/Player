2335377a81c24e3f38ced6ff0d41c2e63262d516
Player37 <31726329+Player37@users.noreply.github.com>
2017-09-11 11:10:34 +0530

    Add files via upload

---
+++ lkj1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+mypos=0
+while mypos<101:
+	 print(2)
+	if mypos==2
+		print(6)
+	if mypos==6:
+	  	print(4)
+	if mypos==4:
+		print(4)
+	if mypos==4:
+		print(3)
+	if mypos==3:
+		print(1)
+
+
+
+            
+
+
